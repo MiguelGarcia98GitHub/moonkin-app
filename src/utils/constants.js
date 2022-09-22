241f1fc2a8339c59039ba901b8780987aae3ab4d
MiguelGarcia98GitHub <100066250+MiguelGarcia98GitHub@users.noreply.github.com>
2022-09-22 16:41:45 +0200

    Added .env file and functionality

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,16 @@
-export const API_URL_access_token =
-	"&access_token=USJrPoH0O9XE6oELvuNXwhA3TLasRMoijy";
+const getToken = async () => {
+	let access_token = "";
+	const TokenResponse = await fetch("https://us.battle.net/oauth/token", {
+		body: "grant_type=client_credentials",
+		headers: {
+			Authorization: `Basic ${import.meta.env.VITE_API_AUTH_CREDENTIALS}`, // encoded to base 64 (needs to be or wont work)
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		method: "POST"
+	}).then(response => (access_token = response.json()));
+	console.log("&access_token=" + TokenResponse.access_token);
+	return "&access_token=" + TokenResponse.access_token;
+};
+
+export const API_URL_access_token = await getToken();
+console.log(import.meta.env.VITE_API_AUTH_CREDENTIALS);
